4d6daaa3b705977adabeac0b9e5fbaf0e1835ccd
Jaroca124 <jacob.caccamo@gmail.com>
2018-07-11 08:06:29 -0400

    Starting Gulp Integration

---
+++ pl/gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename'),
+    minify = require('gulp-minify'),
+    browserSync = require('browser-sync').create(),
+    exec = require('child_process').exec;
+
+gulp.task('default', defaultTask);
+
+function defaultTask(done) {
+  // place code for your default task here
+  done();
+}
+    
